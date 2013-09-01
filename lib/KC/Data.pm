451c8bca14ffd5c243ac21b95d4621de7da81fcd
Chris Cormack <chris@bigballofwax.co.nz>
2013-09-01 09:27:36 +0000

    Continuing with the tidy up

--- lib/KC/Data.pm
+++ lib/KC/Data.pm
@@ -7,6 +7,7 @@ use warnings;
 use LWP::Simple qw($ua get);
 use XML::Simple;
 use DateTime;
+use DBI;
 
 require Exporter;
 
@@ -18,7 +19,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = (
     'all' => [
         qw(
-          get_dates get_devs ohloh_activity
+          get_dates get_devs ohloh_activity last5signoffs
           )
     ]
 );
@@ -30,6 +31,19 @@ our @EXPORT = qw(
 );
 
 our $VERSION = '0.01';
+
+sub last5signoffs {
+    my $database = shift;
+    my $sql =
+"SELECT realname,bugs.bug_id,bug_when,short_desc                                            
+    FROM bugs_activity,profiles,bugs WHERE bugs_activity.who=profiles.userid
+      AND bugs.bug_id=bugs_activity.bug_id AND added='Signed Off' 
+        ORDER BY bug_when DESC LIMIT 5";
+    my $sth = database->prepare($sql) or die database->errstr;
+    $sth->execute or die $sth->errstr;
+    my $entries = $sth->fetchall_arrayref;
+    return $entries;
+}
 
 sub get_dates {
 
@@ -67,6 +81,7 @@ sub get_devs {
 sub ohloh_activity {
     my $url =
 "http://www.ohloh.net/projects/koha/analyses/latest/activity_facts.xml?api_key=ad98f4080e21c596b62c9315f6c7a4c8b08af082";
+
     # get the url from the server
     my $response = get $url or return;
 
